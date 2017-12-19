923c88b2734d2d2d9cd580477e8292d619a067a2
Natasha Jones <tasha.k.jones@gmail.com>
2017-12-19 18:54:56 -0500

    Add spec for endpoint

---
+++ spec/requests/api/v1/reports_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Api::V1::ReportsController' do
+
+  context 'POST api/v1/reports' do
+
+    let(:json) { JSON.parse(response.body) }
+    let(:params) { "Baltimore" }
+
+    subject(:create_report_request) { post "/api/v1/reports",
+                                      params: params }
+
+    before { create_report_request }
+
+    it 'returns the expected response' do
+      expect(json).to match({ english: "It's not so great out there",
+                              details: {
+                                carbon_monoxide: high, 
+                                sulfur_dioxide: low, 
+                                particulate_matter_under_25: medium,
+                                temperature: dangerously_cold
+                              }
+                            }) 
+    end
+
+  end
+
+end
